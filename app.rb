9bee9a2020e33ddf4ae31bc35368705cff7832e9
Alex Loren <lorenalexm@gmail.com>
2016-02-26 04:22:45 -0800

    Set Slim configuration for HTML formatting during production and development

--- app.rb
+++ app.rb
@@ -1,13 +1,16 @@
 require 'sinatra/base'
+require 'slim'
 require_relative 'routes/init'
 
 class FindaPw < Sinatra::Base
   configure :production do
+    Slim::Engine.set_options pretty: false
     set :clean_trace, true
     set :show_exceptions, false
   end
 
   configure :development do
+    Slim::Engine.set_options pretty: true
     set :show_exceptions, true
   end
 
