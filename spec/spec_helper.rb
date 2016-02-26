48920dc81e62158755f56331ff5fe8fdff3b6f82
Alex Loren <lorenalexm@gmail.com>
2016-02-26 01:51:07 -0800

    Removed app method from RSpecMixin module

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -4,16 +4,13 @@ require 'rack/test'
 
 module RSpecMixin
   include Rack::Test::Methods
-
-  def app
-  end
 end
 
 RSpec.configure do |config|
   config.color = true
   config.tty = true
   config.formatter = :documentation
-  
+
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
   end
