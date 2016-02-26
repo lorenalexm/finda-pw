3351d2fb6498d8895415a7cbccb2828902f79b18
Alex Loren <lorenalexm@gmail.com>
2016-02-26 01:45:09 -0800

    Created RSpec helper class within spec folder

---
+++ spec/spec_helper.rb
@@ -0,0 +1,26 @@
+ENV['RACK_ENV'] = 'test'
+
+require 'rack/test'
+
+module RSpecMixin
+  include Rack::Test::Methods
+
+  def app
+  end
+end
+
+RSpec.configure do |config|
+  config.color = true
+  config.tty = true
+  config.formatter = :documentation
+  
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.include RSpecMixin
+end
