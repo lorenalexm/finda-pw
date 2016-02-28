03563014fb9fdcda1c43bca67cd4f444544de320
Alex Loren <lorenalexm@gmail.com>
2016-02-28 03:20:53 -0800

    Updated web spec to use contexts

--- spec/web_spec.rb
+++ spec/web_spec.rb
@@ -1,9 +1,12 @@
 require 'rspec'
+require 'json'
 require_relative 'spec_helper'
 
-describe 'Web facing application', :type => :feature do
-  it 'should display copyright on homepage' do
-    visit '/'
-    expect(page).to have_content 'Copyright 2016'
+describe 'Web facing', :type => :feature do
+  context 'application' do
+    it 'should display copyright on homepage' do
+      visit '/'
+      expect(page).to have_content 'Copyright 2016'
+    end
   end
 end
