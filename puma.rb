dd9e7eaa116653ccc69c2b55570deb18eba30533
Alex Loren <lorenalexm@gmail.com>
2016-03-19 10:40:27 -0700

    Fixed whitespaces

--- puma.rb
+++ puma.rb
@@ -2,6 +2,6 @@ workers 1
 threads_count = Integer(ENV['MAX_THREADS'] || 5)
 threads threads_count, threads_count
 
-rackup      DefaultRackup
-port        ENV['PORT'] || 9292
+rackup DefaultRackup
+port ENV['PORT'] || 9292
 environment ENV['RACK_ENV'] || 'development'
